7c95b68fc3d754181b22451358fb186904f47800
Lamiaa El Ouatili <lamiaaelouatili@hotmail.com>
2025-12-19 17:19:50 +0100

    updated code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from heuristic import *
 from helper import *
 
-VG, EG = read_graph(r"Spd_Inst_Rid_Final2\Spd_RF2_20_27_211.txt")
+test_file = "Spd_Inst_Rid_Final2\Spd_RF2_20_27_211.txt"
+
+VG, EG = read_graph(test_file)
 final_edges, solution_info = iterative_mbvst_with_lp_file(VG, EG, max_iter=50, randomize=False)
